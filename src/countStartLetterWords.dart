e8ee03ff7d94ceb97ca82f828a98efbea453112d
Jufebrown <jufebrown@gmail.com>
2019-04-09 12:28:32 -0500

    make WordCounter class with methods for counting words that start with a letter, end with a letter and both start and end with different letters

--- src/countStartLetterWords.dart
+++ src/countStartLetterWords.dart
@@ -1,9 +1,31 @@
-int countStartLetterWords(textArray, letter) {
-  int wordCounter = 0;
-  for (var i = 0; i < textArray.length; i++) {
-    if (textArray[i].startsWith(letter)) {
-      wordCounter += 1;
+class WordCounter {
+  int countStartLetterWords(textArray, letter) {
+    int wordsCounter = 0;
+    for (var i = 0; i < textArray.length; i++) {
+      if (textArray[i].startsWith(letter)) {
+        wordsCounter += 1;
+      }
     }
+    return wordsCounter;
   }
-  return wordCounter;
+
+  int countEndLetterWords(textArray, letter) {
+    int wordsCounter = 0;
+    for (var i = 0; i < textArray.length; i++) {
+      if (textArray[i].endsWith(letter)) {
+        wordsCounter += 1;
+      }
+    }
+    return wordsCounter;
+  }
+
+  int countStartAndEndLetterWords(textArray, startLetter, endLetter) {
+    int wordsCounter = 0;
+    for (var i = 0; i < textArray.length; i++) {
+      if (textArray[i].startsWith(startLetter) && textArray[i].endsWith(endLetter)) {
+        wordsCounter += 1;
+      }
+    }
+    return wordsCounter;
+  }
 }
